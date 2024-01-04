cd1753abbee96da1cea5e0c6bf3ed148455f9b97
Yash <sharmayash380@gmail.com>
2024-01-05 00:39:00 +0530

    main part of home page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Navbar from "../../navbar1";
 import "./App.css";
+import Main from "./components/Main";
 function App() {
   return (
     <div className=" w-full">
       <Navbar />
+      <Main />
     </div>
   );
 }
---
+++ src/components/Main.js
@@ -0,0 +1,88 @@
+import { useEffect, useState } from "react";
+
+const Main = () => {
+  const [quoteLoading, setQuoteLoading] = useState(false);
+  const [randomQuote, setRandomQuote] = useState("");
+  const [randomQuoteAuthor, setRandomQuoteAuthor] = useState("");
+  const [tagQuery, setTagQuery] = useState("");
+  const [options, setOptions] = useState([]);
+
+  async function getRandomQuote() {
+    setQuoteLoading(true);
+    fetch("https://api.quotable.io/random?tags=" + tagQuery)
+      .then((res) => res.json())
+      .then((data) => {
+        setRandomQuote(data.content);
+        setRandomQuoteAuthor(data.author);
+        setQuoteLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        getRandomQuote();
+      });
+  }
+
+  const handleTagChange = (event) => {
+    const selectedTag = event.target.value;
+    setTagQuery(selectedTag);
+  };
+
+  useEffect(() => {
+    fetch("https://api.quotable.io/tags")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setOptions(data);
+      })
+      .catch((error) => console.error("Error fetching options:", error));
+  }, []);
+
+  useEffect(() => {
+    getRandomQuote();
+  }, [tagQuery]);
+
+  return (
+    <div className="flex flex-col w-full items-center gap-12">
+      <div className=" w-3/5 min-h-[30vh] m-auto mt-28 bg-slate-600 rounded-lg text-white px-16 py-10">
+        {quoteLoading ? (
+          <img src="./loading.svg" alt="" className=" m-auto" />
+        ) : (
+          <>
+            <p className="text-center text-4xl font-semibold">{randomQuote}</p>
+
+            <div className="flex justify-end gap-5 mt-10 items-center  w-full">
+              <h1 className=" text-xl">-{randomQuoteAuthor}</h1>
+              <h4>bookmark</h4>
+            </div>
+          </>
+        )}
+      </div>
+      {/* <i className="fa-solid fa-spinner"></i> */}
+
+      <select
+        name="tags-selector"
+        id="tags-selector"
+        className="block appearance-none w-3/12 text-xl h-10 py-2 px-4 pr-8 leading-tight border border-gray-300 rounded focus:outline-none focus:border-blue-500"
+        onChange={handleTagChange}
+      >
+        <option value="" disabled selected>
+          Select an option
+        </option>
+        {options.map((option) => (
+          <option key={option._id} value={option.slug}>
+            {option.name}
+          </option>
+        ))}
+      </select>
+
+      <button
+        onClick={getRandomQuote}
+        className="px-5 py-2 bg-black text-white text-xl rounded "
+      >
+        Generate another
+      </button>
+    </div>
+  );
+};
+
+export default Main;
