498802295d5173967bcc9bb1c20927dc87014ea3
Yash <sharmayash380@gmail.com>
2024-01-05 00:39:53 +0530

    added routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import Navbar from "../../navbar1";
 import "./App.css";
 import Main from "./components/Main";
+import Navbar from "./components/Navbar";
+import { BrowserRouter, Routes,Route } from "react-router-dom";
 function App() {
   return (
+    <BrowserRouter>
     <div className=" w-full">
       <Navbar />
-      <Main />
+      <Routes>
+        <Route path="/" element={<Main />} />
+      </Routes>      
     </div>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
+
